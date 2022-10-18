e04091db492094e639f49d998ce5b99e9344fbdc
Jakub Choroszucha <jakub.rrk@outlook.com>
2022-10-18 14:22:24 +0200

    add change grid size

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 let container = document.querySelector("#container");
 
-for (let i = 0; i < 16; i++) {
-  let column = document.createElement("div");
-  column.className = "column";
-  container.appendChild(column);
-  for (let j = 0; j < 16; j++) {
-    let cell = document.createElement("div")
-    cell.className = "cell";
-    column.appendChild(cell);
-  }
-}
+makeGrid(32);
 
-let cells = document.querySelectorAll(".cell");
-
-cells.forEach(cell => {
-  cell.addEventListener("mouseover", () => {
-    cell.className = "cell-hovered";
-  })
+let button = document.querySelector("button");
+button.addEventListener("click", (e) => {
+  let size = prompt("give size:", 16);
+  makeGrid(size);
 })
+
+
+
+
+function makeGrid(size) {
+  container.innerHTML = "";
+  for (let i = 0; i < size; i++) {
+    let column = document.createElement("div");
+    column.className = "column";
+    container.appendChild(column);
+    for (let j = 0; j < size; j++) {
+      let cell = document.createElement("div")
+      cell.className = "cell";
+      column.appendChild(cell);
+    }
+  }
+  let cells = document.querySelectorAll(".cell")
+  cells.forEach(cell => {
+    cell.addEventListener("mouseover", () => {
+      cell.className = "cell-hovered";
+    })
+  })
+}
